34189994495a48c929244f034ec8cd40a40085f0
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-05-19 17:35:02 +0900

    カラースキームの設定を移植

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,11 @@
+return {
+  -- カラースキーム
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+}
