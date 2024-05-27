f08e3c8cdad215c2e0c0aeae5079e7a9fbe0fd53
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-05-27 18:24:12 +0900

    ステータスラインのプラグインをlualine.nvimに変更

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -11,42 +11,27 @@ return {
 
   -- ステータスラインカスタマイズ
   {
-    'itchyny/lightline.vim',
-    config = function()
-      vim.cmd([[
-      function! LightlineFugitive()
-        let l:res = ''
-
-        let l:b = FugitiveHead()
-        if strlen(b) > 0
-          let res = ' '.l:b
-        endif
-
-        return res
-      endfunction
-      ]])
-
-      vim.g.lightline = {
-        colorscheme = 'tokyonight',
-        component_function = {
-          gitbranch = 'LightlineFugitive'
-        },
-        active = {
-          left = {
-            {'mode', 'paste'},
-            {'gitbranch', 'readonly', 'filename', 'modified'},
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        section_separators = {left = nil, right = nil},
+        component_separators = {left = '|', right = '|'},
+        theme = 'tokyonight',
+      },
+      sections = {
+        lualine_x = {
+          'encoding',
+          {
+            'fileformat',
+            icons_enabled = false,
           },
-          right = {
-            {'lineinfo'},
-            {'percent'},
-            {'fileformat', 'fileencoding', 'filetype'},
-          },
+          'filetype',
         },
-      }
-    end,
+      },
+    },
     dependencies = {
       'folke/tokyonight.nvim',
-      'tpope/vim-fugitive',
+      'nvim-tree/nvim-web-devicons',
     },
   },
 
