e04dd446c8976bedbde422f38dad558a3c811bb2
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-05-30 00:15:55 +0000

    nvim-scrollbarのmarksの空白を削除

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -69,12 +69,12 @@ return {
           color = colors.bg_highlight,
         },
         marks = {
-          Search = { color = colors.orange },
-          Error = { color = colors.error },
-          Warn = { color = colors.warning },
-          Info = { color = colors.info },
-          Hint = { color = colors.hint },
-          Misc = { color = colors.purple },
+          Search = {color = colors.orange},
+          Error = {color = colors.error},
+          Warn = {color = colors.warning},
+          Info = {color = colors.info},
+          Hint = {color = colors.hint},
+          Misc = {color = colors.purple},
         },
       })
     end,
