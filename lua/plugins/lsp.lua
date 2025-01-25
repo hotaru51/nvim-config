165133a88b2bc45f71b09ef97dff86161ec67742
hotaru <36354847+hotaru51@users.noreply.github.com>
2025-01-25 03:11:50 +0000

    feat: スニペット展開用のプラグイン、設定を追加

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -83,6 +83,7 @@ return {
     config = function()
       local cmp = require('cmp')
       local lspkind = require('lspkind')
+      local luasnip = require('luasnip')
 
       cmp.setup({
         formatting = {
@@ -98,12 +99,20 @@ return {
           }),
         },
 
+        -- スニペット
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+
         -- 補完ソース
         sources = cmp.config.sources({
           {name = 'nvim_lsp'},
           {name = 'buffer'},
           {name = 'path'},
           {name = 'nvim_lsp_signature_help'},
+          {name = 'luasnip'},
         }),
 
         -- キーマッピング
@@ -112,10 +121,15 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
+
           ["<CR>"] = cmp.mapping({
             i = function(fallback)
               if cmp.visible() and cmp.get_active_entry() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                if luasnip.expandable() then
+                  luasnip.expand()
+                else
+                  cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                end
               else
                 fallback()
               end
@@ -123,6 +137,26 @@ return {
             s = cmp.mapping.confirm({ select = true }),
             c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
           }),
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
 
         -- UI設定
@@ -148,6 +182,25 @@ return {
 
   -- nvim-cmpのシグニチャ補完ソース
   'hrsh7th/cmp-nvim-lsp-signature-help',
+
+  -- スニペット集
+  "rafamadriz/friendly-snippets",
+
+  -- スニペットエンジン
+  {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    build = "make install_jsregexp",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+  },
+
+  -- nvim-cmpのLuaSnip補完ソース
+  'saadparwaiz1/cmp_luasnip',
 
   -- Diagnostics用のUI
   {
