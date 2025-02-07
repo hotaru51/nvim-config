a7516e5dd50a6b24228abc3f45204817fdced742
hotaru <36354847+hotaru51@users.noreply.github.com>
2025-02-07 13:15:05 +0000

    feat: telescope.nvimを追加

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -81,5 +81,44 @@ return {
         },
       })
     end,
-  }
+  },
+
+  -- FuzzyFinder
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    config = function()
+      require('telescope').setup({
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = 'ignore_case',
+          },
+        },
+      })
+
+      require('telescope').load_extension('fzf')
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope find files'})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Telescope live grep'})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help tags'})
+      vim.keymap.set('n', '<leader>fr', builtin.resume, {desc = 'Telescope resume'})
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+      'nvim-telescope/telescope-fzf-native.nvim',
+    },
+  },
+
+  -- telescopeのsorter
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make'
+  },
 }
+
+
