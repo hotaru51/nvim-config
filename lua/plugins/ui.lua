08c2fcfc14a0c6fcb8a81dec393947bd8e585486
hotaru <36354847+hotaru51@users.noreply.github.com>
2025-02-11 14:32:34 +0000

    refactor: 起動画面のテキストアートを変更

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -132,7 +132,18 @@ return {
   {
     'goolord/alpha-nvim',
     config = function ()
-      require('alpha').setup(require('alpha.themes.dashboard').config)
+      local dashboard = require('alpha.themes.dashboard')
+      dashboard.section.header.val = {
+        '                                                     ',
+        '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+        '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+        '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+        '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+        '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+        '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+        '                                                     ',
+      }
+      require('alpha').setup(dashboard.opts)
     end,
     dependencies = {
       'echasnovski/mini.icons',
