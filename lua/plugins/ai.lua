aebd45dec590fa2523a596453973a7b96fd0d0a3
hotaru <36354847+hotaru51@users.noreply.github.com>
2025-03-23 12:17:13 +0900

    feat: codecompanion.nvimを追加

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -11,4 +11,27 @@ return {
     end,
     cond = (vim.env.HTR_GH_COPILOT == 'enabled'),
   },
+
+  -- AI ChatやAgent用プラグイン
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "copilot"
+        },
+        inline = {
+          adapter = "copilot"
+        },
+        agent = {
+          adapter = "copilot"
+        },
+      },
+    },
+    cond = (vim.env.HTR_GH_COPILOT == 'enabled'),
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
 }
