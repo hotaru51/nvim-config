84aa2a087e30f62f8991a4493309f9e54a0906d7
hotaru <36354847+hotaru51@users.noreply.github.com>
2025-08-02 08:57:35 +0000

    refactor: hover、signature_help時のborder表示を無理矢理設定

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,6 +12,27 @@ return {
         float = { border = "rounded" },
       })
 
+      -- hover表示時のborder設定
+      -- vim.lsp.hover()をoverrideするように設定
+      local _hover = vim.lsp.buf.hover
+      vim.lsp.buf.hover = function (opts)
+        opts = opts or {}
+        opts.border = opts.border or 'rounded'
+        return _hover(opts)
+      end
+
+      -- signature_help表示時のborder設定
+      -- vim.lsp.signature_help()をoverrideするように設定
+      local _signature_help = vim.lsp.buf.signature_help
+      vim.lsp.buf.signature_help = function(opts)
+        opts = opts or {}
+        opts.border = opts.border or 'rounded'
+        return _signature_help(opts)
+      end
+
+      -- 他のプラグインのUIで問題がでなければ下記でborderを設定する
+      -- vim.o.winborder = 'rounded'
+
       -- :Formatでフォーマットを実行
       vim.api.nvim_create_user_command('Format', function() vim.lsp.buf.format({ async = true }) end, {})
 
