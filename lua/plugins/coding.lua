95bbab600c714595d2dd8b33476a22e95b76a389
hotaru <36354847+hotaru51@users.noreply.github.com>
2026-01-31 12:30:34 +0900

    refactor: インデントを有効化しない言語を変更

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -105,8 +105,9 @@ return {
             vim.wo[0][0].foldmethod = 'expr'
             vim.wo[0][0].foldenable = false
 
-            -- インデントの有効化(一旦markdown以外)
-            local exclude_indent_filetype = { 'markdown' }
+            -- インデントの有効化
+            -- 特定のファイルタイプは有効化させない
+            local exclude_indent_filetype = { 'typescript', 'json', 'jsonc' }
             if not vim.tbl_contains(exclude_indent_filetype, vim.bo.filetype) then
               vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
             end
